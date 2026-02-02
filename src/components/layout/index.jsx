d5bc7d3a2ef3f8a0b1c97cdfe277874611c5e9b5
quandxnunxi28 <quandxnunxi28@gmail.com>
2026-02-02 09:09:58 +0700

    edit frontend

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -3,6 +3,7 @@ import { useAuthStore } from '../../store/authStore';
 import { useCartStore } from '../../store/cartStore';
 import { Menu, X, ShoppingCart, User, LogOut } from 'lucide-react';
 import { useState } from 'react';
+import Logo from './image/img1.jpg';
 
 export function Header() {
   const { user, isAuthenticated, logout } = useAuthStore();
@@ -21,9 +22,13 @@ export function Header() {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center gap-2">
-            <div className="text-3xl">🧸</div>
-            <span className="font-bold text-xl text-gray-900">CuddleBear</span>
-          </Link>
+  <img 
+    src={Logo} 
+    alt="Memo Soft Logo" 
+    className="w-10 h-10 object-contain"
+  />
+  <span className="font-bold text-xl text-gray-900">Memo Soft</span>
+</Link>
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center gap-8">
